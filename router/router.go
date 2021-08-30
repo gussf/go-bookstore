003f6d1552a53fe22ce1479b0e529df5ca32bb82
Gustavo <25396922+gussf@users.noreply.github.com>
2021-08-30 01:35:00 -0300

    Uncomment Router interface methods

--- router/router.go
+++ router/router.go
@@ -7,11 +7,11 @@ import (
 
 type Router interface {
 	ServeHTTP(http.ResponseWriter, *http.Request)
-	// Index(w http.ResponseWriter, r *http.Request)
-	// FindById(w http.ResponseWriter, r *http.Request)
-	// All(w http.ResponseWriter, r *http.Request)
+	Index(w http.ResponseWriter, r *http.Request)
+	FindById(w http.ResponseWriter, r *http.Request)
+	All(w http.ResponseWriter, r *http.Request)
 	Add(w http.ResponseWriter, r *http.Request)
-	// RemoveById(w http.ResponseWriter, r *http.Request)
+	RemoveById(w http.ResponseWriter, r *http.Request)
 }
 
 func WriteResponse(w http.ResponseWriter, v interface{}, statusCode int) {
